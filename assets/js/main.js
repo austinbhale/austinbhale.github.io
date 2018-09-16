23c73c70750d73f5b5b1c5cf67e7c301ecddb9e1
austinbhale <haleau@live.unc.edu>
2018-09-16 18:12:56 -0400

    Added fixed navbar implementation plus contact links.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 var SCROLLSPY_TIME = 500;
+var heightSlider = $('#nav').height();
 
 // Smooth scroll to hash -- selects each used link with hashes.
-$('a[href*="#"]') .not('[href="#"]')
+$('a[href*="#"]') 
+    .not('[href="#"]')
     .not('[href="#0"]')
     .click(function(event) {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
@@ -11,14 +13,14 @@ $('a[href*="#"]') .not('[href="#"]')
             if (target.length) {
                 event.preventDefault();
                 $('html, body').animate({
-                    scrollTop: target.offset().top
+                    scrollTop: target.offset().top - heightSlider
                 }, SCROLLSPY_TIME, function() {
                 var $target = $(target);
                 $target.focus();
                 if ($target.is(":focus")) {
                     return false;
                 } else {
-                    $target.attr('tabindex','-1');
+                    // $target.attr('tabindex','-1');
                     $target.focus();
                 };
                 });
