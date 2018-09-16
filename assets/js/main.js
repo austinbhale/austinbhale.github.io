d11796f83a5dc853b21d20f4bf46d44e3dff03bc
austinbhale <haleau@live.unc.edu>
2018-09-16 15:10:34 -0400

    Initial files

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var SCROLLSPY_TIME = 500;
+
+// Smooth scroll to hash -- selects each used link with hashes.
+$('a[href*="#"]') .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, SCROLLSPY_TIME, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) {
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1');
+                    $target.focus();
+                };
+                });
+            }
+        }
+});
